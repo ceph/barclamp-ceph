fc36925aa9194454d9c448dcb30c63356492681b
Tyler Brekke <tbrekke@gmail.com>
2012-07-12 16:18:09 -0500

    Added moniter-secret script.
    The ceph_service.rb model now autogenerates the monitor secret key attribute when the proposal is created.

---
+++ bin/monitor-secret.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+
+import base64
+import struct
+
+AES_128_KEY_TYPE = 1
+AES_128_KEY_LEN = 16
+
+def encode(created_nsec, entropy):
+    NANO = 1000000000
+    secs, nsecs = divmod(created_nsec, NANO)
+    assert len(entropy) == AES_128_KEY_LEN
+    s = struct.pack(
+        '<HLLH16s',
+        AES_128_KEY_TYPE,
+        secs,
+        nsecs,
+        AES_128_KEY_LEN,
+        entropy,
+        )
+    assert len(s) == 28
+    return base64.b64encode(s)
+
+if __name__ == '__main__':
+    import random
+    import time
+
+    entropy = struct.pack(
+        'QQ',
+        random.getrandbits(64),
+        random.getrandbits(64),
+        )
+    print encode(
+        created_nsec=int(time.time() * 1e9),
+        entropy=entropy,
+        )
--- crowbar_framework/app/models/ceph_service.rb
+++ crowbar_framework/app/models/ceph_service.rb
@@ -26,6 +26,9 @@ class CephService < ServiceObject
 
     base["attributes"]["ceph"]["config"]["fsid"] = `uuidgen`
     
+    #For testing use only. Will be implemeted into the cookbook. 
+    base["attributes"]["ceph"]["config"]["monitor_secret"] = `python /opt/dell/bin/monitor-secret.py`    
+    
     @logger.debug("Ceph create_proposal: exiting")
     base
 
